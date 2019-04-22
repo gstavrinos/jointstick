bc10e8a8503e80b8d6c4c6c03e83126ceece63dd
George Stavrinos <gstavrinos@iit.demokritos.gr>
2019-04-22 17:18:54 +0300

    Added publisher initialisation based on controller type

--- scripts/jointstick.py
+++ scripts/jointstick.py
@@ -1,22 +1,39 @@
 #!/usr/bin/env python
 import os
 import yaml
+import time
 import rospy
+from std_msgs.msg import Float64
+from geometry_msgs.msg import Twist
 from sensor_msgs.msg import Joy, JointState
+from trajectory_msgs.msg import JointTrajectory
 from controller_manager_msgs.srv import ListControllers
 
+# Imports from custom files of this package
+from controllers_info import *
 from helper import Controller, JoyAction
 
 controllers = []
 joint_states = None
+publishers = dict()
 
+# Just the joint state callback
 def jointStatesCallback(msg):
     global joint_states
     joint_states = msg
 
+# Just the joy callback
 def joyCallback(msg):
-    #TODO
-    pass
+    print msg
+
+# Initialize publisher objects for all controller with joy bindings
+def fixPublishers():
+    global publishers
+    for controller in controllers:
+        publishers[controller.name] = rospy.Publisher(
+                        controller.name+topic_extension[controller.type],
+                        controller_type_correspondence[controller.type],
+                        queue_size=1)
 
 # Translate a dict record to a JoyAction object
 def joyActionFromDict(dict_record):
@@ -43,7 +60,7 @@ def main():
     global controllers
     rospy.init_node("jointstick_setup")
     config_file = rospy.get_param("config_file", "-")
-    #config_file = "/home/gstavrinos/config_22-04-2019_15-52-40.yaml"
+    config_file = "/home/gstavrinos/config_22-04-2019_15-52-40.yaml"
     if config_file == "-":
         print("No config file provided. Exiting...")
         return
@@ -67,6 +84,8 @@ def main():
 
     # Delete controllers that do not have any joy bindings
     controllers = [x for x in controllers if x.joyActions]
+
+    fixPublishers()
 
     rospy.Subscriber("/joy", Joy, joyCallback)
     rospy.Subscriber("/joint_states", JointState, jointStatesCallback)
